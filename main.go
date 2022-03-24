46b6563a047aa5a735487e991d638069a3dd61b5
MohineeMuradanar <mmohinee08@gmail.com>
2022-03-24 12:08:41 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"RestAssignment/controller"
+	"fmt"
+)
+
+//go:generate swagger generate spec -m -o ./swagger.json
+
+func main() {
+
+	fmt.Println("Starting the application....")
+
+	controller.Handler()
+
+}
